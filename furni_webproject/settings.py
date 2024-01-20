15fcd501ebeb05592875836e22e0cae4d0fb3cd9
misha <bdemlolp234@gmail.com>
2024-01-20 20:21:00 +0200

    fixed version

--- furni_webproject/settings.py
+++ furni_webproject/settings.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 import secrets
 import dj_database_url
 
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sodqm_!&1sx+i3#7x0te_az6@yf^-@_0j)wmv)zqttxp7=89c0'
+SECRET_KEY = os.environ.get('SECRET_KEY', secrets.token_hex(32))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
 
 
 # Application definition
